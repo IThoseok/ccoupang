2c11d56b6bc88c686143bac3b96c553cd7eb8fba
bhs <qushtj@gmail.com>
2023-03-28 19:45:38 +0900

    Initial commit

---
+++ src/main/java/hta/ccoupang/CcoupangApplication.java
@@ -0,0 +1,13 @@
+package hta.ccoupang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcoupangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcoupangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hta/ccoupang/CcoupangApplicationTests.java
@@ -0,0 +1,13 @@
+package hta.ccoupang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcoupangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
